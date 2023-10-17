a6bd6201de8d81ad4d42cd3629602cd779385fb1
Manelisi Mpotulo <220532826@mycput.ac.za>
2023-10-17 18:46:36 +0200

    update logic game (include icons)

--- logic.js
+++ logic.js
@@ -1,12 +1,12 @@
-const logicDirs = document.querySelectorAll('.logic-dir');
-const playDirs = document.querySelectorAll('.play-dir');
+const dirOptions = document.querySelectorAll('.logic-dir');
+const dirBoxex = document.querySelectorAll('.play-dir');
 const gridCells = document.querySelectorAll('.grid-cell');
 
 //reference play buttons
 const playBtn = document.querySelector('#play-btn');
 const resetBtn = document.querySelector('#reset-btn');
 
-let playDirArr = [];
+let dirBoxArr = [];
 let startCellId = 0
 
 //maize paterns
@@ -15,6 +15,9 @@ const pattern2 = [1, 2, 3, 8, 13]
 const patten3 = [17, 18, 13, 14, 9]
 const pattern4 = [22, 23, 18, 13, 14]
 let currentPattern = pattern1;
+
+//human object
+const humanObj = "<i class=\"fa-solid fa-child-reaching\"></i>";
 
 //randomize choose maize pattern
 let randomNum = Math.floor(Math.random() * 4);
@@ -42,64 +45,66 @@ gridCells.forEach((gridCell) => {
 startCellId = currentPattern[0];
 gridCells.forEach((gridCell) => {
     if (gridCell.id == startCellId) {
-        gridCell.classList.add('active');
-        gridCell.innerText = '*';
+        gridCell.innerHTML = humanObj;
     }
 });
 
-logicDirs.forEach((logicDir) => {
-    logicDir.addEventListener('click', () => {
-        let dir = logicDir.innerText;
-        playDirArr.push(dir);
+/************add choosen directions to the boxes********/
+dirOptions.forEach((dirOption) => {
+    dirOption.addEventListener('click', () => {
+        let dir = dirOption.innerHTML;
+        // get icon from the dirOption
+        let icon = dirOption.querySelector('i');
+        console.log(icon);
+        //get class name of the icon remove the fa-solid class
+        let iconClass = icon.className.split(' ')[1];
+        console.log(iconClass);
+        if (dirBoxArr.length != 4) {
+            dirBoxArr.push(dir);
+        }
 
-        //loop trhough the array and add each element to the playDirs
-        for (let i = 0; i < playDirArr.length; i++) {
-            playDirs[i].innerText = playDirArr[i];
+        //loop trhough the array and add each element to the dirBoxex
+        for (let i = 0; i < dirBoxArr.length; i++) {
+            dirBoxex[i].innerHTML = dirBoxArr[i];
         }
     });
 });
 
 //play button
 playBtn.addEventListener('click', () => {
-    //get the grid cell with an * as innerText
-    gridCells.forEach((gridCell) => {
-        if (gridCell.innerText === '*') {
-            gridCell.classList.add('active');
-            startCellId = gridCell.id;
-            // alert(startCellId);
-        }
-    });
-
-    playLogic(startCellId, playDirArr)
+    playLogic(startCellId, dirBoxArr)
 });
 
 //reset button
 resetBtn.addEventListener('click', () => {
-    playDirArr = [];
-    playDirs.forEach((playDir) => {
-        playDir.innerText = '';
+    dirBoxArr = [];
+    dirBoxex.forEach((dirBox) => {
+        dirBox.innerHTML = '';
 
         //remove the active class from all the grid cells
         gridCells.forEach((gridCell) => {
             if (gridCell.id == startCellId) {
-                gridCell.classList.add('active');
-                gridCell.innerText = '*';
+
+                gridCell.innerHTML = humanObj;
             } else {
                 gridCell.classList.remove('active');
                 gridCell.innerText = '';
             }
-
         });
     });
 });
 
 //play logic
-function playLogic(currentID, playDirArr) {
+function playLogic(currentID, dirBoxArr) {
 
     //loop through the directions array
-    for (let i = 0; i < playDirArr.length; i++) {
+    for (let i = 0; i < dirBoxArr.length; i++) {
 
-        if (playDirArr[i] === '^') {
+        //get the class name of the icon in dirBoxArr
+        let iconClass = dirBoxex[i].querySelector('i').className.split(' ')[1];
+        // console.log(iconClass);
+
+        if (iconClass === 'fa-arrow-up') {
             //move the current cell to the next forward cell
             currentID = parseInt(currentID) + 1;
 
@@ -111,7 +116,7 @@ function playLogic(currentID, playDirArr) {
                 move(currentID)
             }
 
-        } else if (playDirArr[i] === '>') {
+        } else if (iconClass === 'fa-turn-down') {
             //move the current cell to the next down cell
             currentID = parseInt(currentID) + 5;
 
@@ -123,7 +128,7 @@ function playLogic(currentID, playDirArr) {
                 move(currentID)
             }
 
-        } else if (playDirArr[i] === "<") {
+        } else if (iconClass === 'fa-turn-up') {
             //move the current cell to the next up cell
             currentID = parseInt(currentID) - 5;
 
@@ -135,7 +140,6 @@ function playLogic(currentID, playDirArr) {
                 move(currentID)
             }
         }
-
     }
 
     //check for win
@@ -147,15 +151,13 @@ function move(currentID) {
     //set the  the grid cell at current ID to have the text *
     gridCells.forEach((gridCell) => {
         if (gridCell.id === currentID.toString()) {
-            gridCell.classList.add('active');
-            gridCell.innerText = '*';
+            gridCell.innerHTML = humanObj;
         } else {
             gridCell.classList.remove('active');
-            gridCell.innerText = '';
+            gridCell.innerHTML = '';
         }
     });
 }
-
 
 //check for win function
 function checkForWin(currentID) {
